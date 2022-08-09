b7ff34abeb74d424654579db727f3ec66230de60
Ogundare Samuel <oluwadareysamuel@yahoo.com>
2022-08-09 07:31:39 +0100

    🎉  feat: initial setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import { Box} from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Box>
+      <Head>
+        <title> Messages</title>
+      </Head>
+   <Box>Home</Box>
+    </Box>
+  );
+}
